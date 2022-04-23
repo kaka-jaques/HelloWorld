45cd445830cb28d247d242a2a44d4bdd1dd89f5f
kaka-jaques <kjfakhouri@gmail.com>
2022-04-23 15:00:19 -0300

    gited, finally

---
+++ src/helloworld/kalil/entra21/com/br/Main.java
@@ -0,0 +1,13 @@
+package helloworld.kalil.entra21.com.br;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Welcome to my first Hello World! Click OK!");
+		
+	}
+
+}
