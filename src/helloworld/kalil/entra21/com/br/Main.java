217fb258b0508741a59680bcfaac90eeec88c5e5
kaka-jaques <kjfakhouri@gmail.com>
2022-04-26 19:27:37 -0300

    Variable information added

--- src/helloworld/kalil/entra21/com/br/Main.java
+++ src/helloworld/kalil/entra21/com/br/Main.java
@@ -8,6 +8,14 @@ public class Main {
 		
 		JOptionPane.showMessageDialog(null, "Welcome to my first Hello World! Click OK!");
 		
+		System.out.println("****VARIABLES INFORMATION****\n");
+		System.out.println("The Value of Byte is: "+Byte.MIN_VALUE+" - "+Byte.MAX_VALUE);
+		System.out.println("The Value of Short is: "+Short.MIN_VALUE+" - "+Short.MAX_VALUE);
+		System.out.println("The Value of Int is: "+Integer.MIN_VALUE+" - "+Integer.MAX_VALUE);
+		System.out.println("The Value of Double is: "+Double.MIN_VALUE+" - "+Double.MAX_VALUE);
+		System.out.println("The Value of Float is: "+Float.MIN_VALUE+" - "+Float.MAX_VALUE);
+		System.out.println("The Value of Long is: "+Long.MIN_VALUE+" - "+Long.MAX_VALUE);
+		
 	}
 
 }
